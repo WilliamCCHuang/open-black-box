bc8455dfeb71e7b6db0e3c871a95d7098ad8cab5
williamhuang <hang6318179@gmail.com>
2020-03-29 00:29:08 +0800

    [MODIFY]

--- CAM/utils.py
+++ CAM/utils.py
@@ -2,7 +2,7 @@ import json
 from PIL import Image
 
 import torch
-import torchvision.transforms as transforms
+from torchvision import transforms
 
 
 def load_image(img_path):
@@ -13,15 +13,16 @@ def load_image(img_path):
     return img
     
 
-def preprocess_image(img):
+def preprocess_image(img, mean, std):
     img_tensor = transforms.ToTensor()(img)
+    img_tensor = transforms.Normalize(mean, std)(img_tensor)
     img_tensor = torch.unsqueeze(img_tensor, dim=0)  # (1, 3, 299, 299)
     
     return img_tensor
     
 
 def load_class_idx():
-    with open('../../imagenet_class_index.json') as f:
+    with open('../imagenet_class_index.json') as f:
         class_idx = json.load(f)
     
     idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
